5d0a7fee5c969308cab4e8bfcb0e8ebb99958593
numraise <num@raiseinnovation.com>
2022-07-26 00:08:28 +0700

    tell num2 super

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 player.onChat("com2", function () {
     mobs.spawn(SHEEP, player.position())
-    player.tell(mobs.target(ALL_PLAYERS), "[name=num2] super")
+    player.execute(
+    "/tell num2 super"
+    )
     player.say(":)")
 })
